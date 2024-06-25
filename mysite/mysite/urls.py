ef11d6f29e2d3860704f67925ad4e00dff34f511
Subha Vadlamannati <subha@Subhas-Laptop.local>
2024-06-25 14:52:19 -0700

    adding comments

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+# Mysite is the giant root directory, and all these are directions for where the urls should go
+# For example, the path polls should go to polls.urls to see further directions
+# Every new path should use include("smth"), the admin one is special so its different
+
 urlpatterns = [
-    path("polls/", include("polls.urls")),
+    path("polls/", include("polls.urls")), 
     path("admin/", admin.site.urls),
 ]
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+# if you add a url here, it would go to localhost/polls/results for example if you added results
+
 urlpatterns = [
     path("", views.index, name="index"),
 ]
